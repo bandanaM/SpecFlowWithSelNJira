e1f549c4106a11e93e1879704db35d908803c7df
bandanaM <40889510+bandanaM@users.noreply.github.com>
2018-07-19 14:56:58 +0530

    Add project files.

---
+++ SpecFlowWithSelNJira/Features/2.feature
@@ -0,0 +1,15 @@
+﻿Feature: Validate Error in Login Page
+
+Scenario: validate Error in Login Page
+	Given I enter url in Browser
+	Then I enter UserName in username field
+	| UserName  |
+	| bandana.m |
+	Then I enter Password in password field
+	| Password  |
+	| Ganesh@23 |
+     Then I select Domain from domain dropdown
+	| Domain  |
+	| HCLTECH |
+	Then Click submit button
+	Then verify Login Error Message
---
+++ SpecFlowWithSelNJira/StepDefination.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace SpecFlowWithSelNJira.StepDefinationFiles
+{
+    [Binding]
+    class StepDefination
+    {
+        private IWebDriver driver;
+        public StepDefination(IWebDriver _driver)
+        {
+            driver = _driver;
+        }
+        [Given(@"I enter url in Browser")]
+        public void GivenIEnterUrlInBrowser()
+        {
+            driver.Navigate().GoToUrl("http://myhcl.com");
+        }
+
+        [Then(@"I enter UserName in username field")]
+        public void ThenIEnterUserNameInUsernameField(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            driver.FindElement(By.Id("txtUserID")).SendKeys((String)data.UserName);
+        }
+
+        [Then(@"I enter Password in password field")]
+        public void ThenIEnterPasswordInPasswordField(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            driver.FindElement(By.Id("txtPassword")).SendKeys((String)data.Password);
+        }
+
+        [Then(@"I select Domain from domain dropdown")]
+        public void ThenISelectDomainFromDomainDropdown(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            SelectElement oselect = new SelectElement(driver.FindElement(By.Id("ddlDomain")));
+            oselect.SelectByText((String)data.Domain);
+        }
+
+        [Then(@"Click submit button")]
+        public void ThenClickSubmitButton()
+        {
+            driver.FindElement(By.Id("btnSubmit")).Click();
+        }
+
+        [Then(@"verify Welcome Page")]
+        public void ThenVerifyWelcomePage()
+        {
+            var element = driver.FindElement(By.ClassName("userName"));
+            Assert.That(element.Text, Is.Not.Null, "Header Text is not Found");
+
+
+        }
+        [Then(@"verify Login Error Message")]
+        public void ThenVerifyLoginErrorMessage()
+        {
+            var element = driver.FindElement(By.Id("lblMsg"));
+            Assert.That(element.Text, Is.Not.Null, "Header Text is not Found");
+
+        }
+    }
+}
